a5c7f61065195b9bb2c97c48215881e923628178
Benjamin Alcala <benalcalajr@gmail.com>
2019-03-24 16:14:42 -0700

    fixed reference to database object that doesn't exist

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
-app.get('/', (req, res) => {res.json(database.users)});
+app.get('/', (req, res) => {res.json('It is working!')});
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
